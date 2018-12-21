7ae4ba49db7069c93c4158d8138abcb460b106c6
Barbara Washington <barbara.m.washington@gmail.com>
2018-12-21 20:26:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = 'Babs';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = 'Welcome to Flatbook, ' ${currentUser};
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, BABS!';
 const shortGreeting = "Welcome, B!"
